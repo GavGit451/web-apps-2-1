b195a695f1bb4754cd88c544bd7a7e07c560fa43
Gavin Pavlick <gavin.pavlick@kellogg.northwestern.edu>
2023-05-11 01:21:10 +0000

    next commit

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,14 +1,13 @@
 class CompaniesController < ApplicationController
 
   def index
-    # find all Company rows
-    # render companies/index view
   end
 
-  # def show
+  def show
   #   # find a Company
+    @company = Company.find_by({"id" => params["id]})"]})
   #   # render companies/show view with details about Company
-  # end
+  end
 
   # def new
   #   # render view with new Company form
